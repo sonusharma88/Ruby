75bda14732e882f59f7355283d3c159654ad208a
Sonu Sharma <sonusharmacse88@gmail.com>
2021-07-05 09:19:27 +0530

    Initial commit

---
+++ src/Poter.java
@@ -0,0 +1,8 @@
+public class Poter {
+    public static void main(String[] args){
+        System.out.println("kishan pal singh");
+        System.out.println("yogendra kumar");
+        System.out.println("sonu sharma");
+    }
+}
+
